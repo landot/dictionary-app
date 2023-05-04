a5d71f187eb586506f8e16176cc760fdf280f0c4
landot <timowland@gmail.com>
2023-05-04 13:07:35 -0700

    feat: add react router and set url word param during a search

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useEffect, useState } from 'react';
-import { getDictionaryResults } from './api';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Page } from './Page';
 import './App.css'
-import { DictionaryApiResponse } from './assets/ApiResponse';
-import { Font } from './assets/fonts';
-import { Header } from './components/Header';
-import { SearchField } from './components/SearchField';
-import { SearchResults } from './components/SearchResults';
 
 
-// - Play the audio file for a word when it's available
 // - See hover and focus states for all interactive elements on the page
-// add url search parameters
 // add hyperlinks to synonyms and antonyms using url search parameters
 // clean up desktop styling
 // clean up mobile styling
@@ -24,71 +17,12 @@ import { SearchResults } from './components/SearchResults';
 // https://api.dictionaryapi.dev/api/v2/entries/en/hello
 
 function App() {
-    const [theme, setTheme] = useState<string>(localStorage.getItem('theme') || 'light');
-    const [font, setFont] = useState<string>(localStorage.getItem('font') || Font.Inter);
-    const [search, setSearch] = useState<string>('');
-    const [searchResults, setSearchResults] = useState<DictionaryApiResponse[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string>('');
-
-    const toggleTheme = () => {
-      if (theme === 'light') {
-        setTheme('dark');
-      } else {
-        setTheme('light');
-      }
-    };
-
-    const toggleFont = () => {
-      if(font === Font.Inconsolata) {
-        setFont(Font.Inter)
-      } else if(font === Font.Inter) {
-        setFont(Font.Lora)
-      } else {
-        setFont(Font.Inconsolata)
-      }
-    }
-
-    const handleSearch = (searchText: string) => {
-      setSearch(searchText);
-      setSearchResults([]);
-    }
-
-    useEffect(() => {
-      localStorage.setItem('theme', theme);
-      localStorage.setItem('font', font);
-      document.body.className = `${theme} ${font}`;
-    }, [theme, font]);
-
-    useEffect(() => {
-      async function loadSearchResults() {
-        setLoading(true);
-        setError('');
-
-        try {
-          const data = await getDictionaryResults(search);
-          setSearchResults(data);
-        } catch (err: any) {
-          setError(err.message);
-        } finally {
-          setLoading(false);
-        }
-      }
-      if(search) {
-        loadSearchResults();
-      }
-    }, [search]);
-
   return (
-    <div className={`App ${theme}`}>
-      <div className='content'>
-        <Header theme={theme} toggleTheme={toggleTheme} toggleFont={toggleFont}/>
-        <SearchField theme={theme} handleSearch={handleSearch}/>
-        {!loading && 
-          <SearchResults theme={theme} searchResults={searchResults} errorMessage={error}/>
-        }
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Page />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
